6553811f4feff7b046711160ea4cada40dfb79b6
Tommy Liu <tommyjqliu@outlook.com>
2024-10-28 09:22:55 +0800

    fix: vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        outputFileTracingExcludes: {
+            '*': [
+                'node_modules/@swc/core-linux-x64-gnu',
+                'node_modules/@swc/core-linux-x64-musl',
+                'node_modules/@esbuild/linux-x64',
+            ],
+        },
+    },
+};
 
 if (process.env.NODE_ENV === 'development') {
     nextConfig.rewrites = async () => {
